644558f21ea620e69d6ef338c03e99775fea4ffc
manoj bhatt <enterbhatt123@gmail.com>
2025-08-05 17:03:39 +0530

    WIP: Implementing swiper integration

---
+++ src/components/Slider.tsx
@@ -0,0 +1,5 @@
+import { Swiper, SwiperSlide } from "swiper/react";
+import 'swiper/swiper-bundle.min.css';
+import 'swiper/swiper.min.css';
+import 'swiper/components/navigation/navigation.min.css'
+import 'swiper/components/pagination/pagination.min.css'
