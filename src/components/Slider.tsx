c3d006ebf45ecbf037f1f58ecc5ba00afb42cf47
manoj bhatt <enterbhatt123@gmail.com>
2025-08-06 15:48:51 +0530

    feat: slider working perfectly

--- src/components/Slider.tsx
+++ src/components/Slider.tsx
@@ -1,5 +1,35 @@
 import { Swiper, SwiperSlide } from "swiper/react";
-import 'swiper/swiper-bundle.min.css';
-import 'swiper/swiper.min.css';
-import 'swiper/components/navigation/navigation.min.css'
-import 'swiper/components/pagination/pagination.min.css'
+import "swiper/swiper-bundle.css";
+import { Navigation, Pagination, Autoplay } from "swiper/modules";
+
+const Slider: React.FC = () => {
+  return (
+    <Swiper
+      modules={[Navigation, Pagination, Autoplay]}
+      autoplay={{ delay: 1900, disableOnInteraction: false }}
+      loop={true}
+      navigation
+      pagination={{ clickable: true }}
+      spaceBetween={0}
+      slidesPerView={1}
+    >
+      <SwiperSlide>
+        <img src="/banner1.webp" alt="" />
+      </SwiperSlide>
+      <SwiperSlide>
+        <img src="/banner2.webp" />
+      </SwiperSlide>
+      <SwiperSlide>
+        <img src="/banner3.webp" />
+      </SwiperSlide>
+      <SwiperSlide>
+        <img src="/banner4.webp" />
+      </SwiperSlide>
+      <SwiperSlide>
+        <img src="/banner5.webp" />
+      </SwiperSlide>
+    </Swiper>
+  );
+};
+
+export default Slider;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,9 @@
 import Header from "../components/Header";
+import Slider from "../components/Slider";
 const Home = ()=>{
     return <>
         <Header></Header>
+        <Slider></Slider>
     </>
 }
 
