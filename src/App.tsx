32f91a3c2513c5ac841a9bbd56f6604a9c0a7fed
manoj bhatt <enterbhatt123@gmail.com>
2025-08-26 17:07:48 +0530

    feat: user login now stable over refresh as well

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,18 @@ import "./App.css";
 import Home from "./pages/Home";
 import ProductPage from "./pages/ProductPage"; 
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-
+// import { UseDispatch } from "react-redux";
+import { useDispatch } from "react-redux";
+import { setLogin } from "./store/authSlice";
 function App() {
-  return (
+  const dispatch = useDispatch()
+  const uid = localStorage.getItem("uid") || "";
+  const name = localStorage.getItem("name") || "";
+  const email = localStorage.getItem("email");
+  if(uid && email){
+    dispatch(setLogin({uid, name,email}))
+  }
+  return ( 
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
--- src/components/LoginForm.tsx
+++ src/components/LoginForm.tsx
@@ -1,9 +1,10 @@
 import { useState, useRef } from "react";
 import SignUp from "../pages/SignUp.tsx";
 import Modal from "./Modal";
-import { doc, getDoc } from "firebase/firestore";
-import { auth, db } from "../firebaseConfig.tsx";
-import { signInWithEmailAndPassword } from "firebase/auth";
+import { firebaseConfig } from "../firebaseConfig.tsx";
+// import { doc, getDoc } from "firebase/firestore";
+// import { auth, db } from "../firebaseConfig.tsx";
+// import { signInWithEmailAndPassword } from "firebase/auth";
 import { useSelector, useDispatch } from "react-redux";
 import { setLogin } from "../store/authSlice.tsx";
 interface LoginProps {
@@ -28,36 +29,85 @@ const LoginForm = ({ onClose }: LoginProps) => {
       return;
     }
     try {
-      const userCredential = await signInWithEmailAndPassword(
-        auth,
-        email,
-        password
-      );
-      console.log("user credentials", userCredential);
-      const user = userCredential.user;
-      const docRef = doc(db, "users", user.uid);
-      const docSnap = await getDoc(docRef);
-      console.log("docsnap", docSnap)
-      if (docSnap.exists()) {
-        console.log("User profile:", docSnap.data());
-        const uid = userCredential.user.uid;
-        const name = docSnap.data().name;
-        const email = docSnap.data().email;
+      // const userCredential = await signInWithEmailAndPassword(
+      //   auth,
+      //   email,
+      //   password
+      // );
+      // console.log("user credentials", userCredential);
+      // const user = userCredential.user;
+      // const docRef = doc(db, "users", user.uid);
+      // const docSnap = await getDoc(docRef);
+      // console.log("docsnap", docSnap)
+      // if (docSnap.exists()) {
+      //   console.log("User profile:", docSnap.data());
+      //   const uid = userCredential.user.uid;
+      //   const name = docSnap.data().name;
+      //   const email = docSnap.data().email;
 
-        console.log("user data to dispatch", uid, name, email);
-        dispatch(
-          setLogin({
-            uid,
-            name,
-            email,
-          })
-        );
-        alert(`Welcome ${docSnap.data().name}!`);
-      } else {
-        console.warn("No profile found for user");
-        
-        alert("no profile found");
+      //   console.log("user data to dispatch", uid, name, email);
+      //   dispatch(
+      //     setLogin({
+      //       uid,
+      //       name,
+      //       email,
+      //     })
+      //   );
+      //   alert(`Welcome ${docSnap.data().name}!`);
+      // } else {
+      //   console.warn("No profile found for user");
+
+      //   alert("no profile found");
+      // }
+      // onClose();
+      const url = `https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${firebaseConfig.apiKey}`;
+
+      const response = await fetch(url, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          email,
+          password,
+          returnSecureToken: true,
+        }),
+      });
+
+      if (!response.ok) {
+        const err = await response.json();
+        throw new Error(err.error.message);
       }
+
+      const logInData = await response.json();
+      console.log("Login success:", logInData);
+      localStorage.setItem("idToken", logInData.idToken);
+      const uid = logInData.localId;
+      const idToken = logInData.idToken;
+      const userDocUrl = `https://firestore.googleapis.com/v1/projects/${firebaseConfig.projectId}/databases/(default)/documents/users/${uid}`;
+
+      const userDocRes = await fetch(userDocUrl, {
+        method: "GET",
+        headers: {
+          Authorization: `Bearer ${idToken}`,
+        },
+      });
+
+      if (!userDocRes.ok) {
+        alert("No profile found for user");
+        return;
+      }
+
+      const userDoc = await userDocRes.json();
+      console.log("User doc:", userDoc);
+
+      const name = userDoc.fields?.name?.stringValue;
+      const userEmail = userDoc.fields?.email?.stringValue;
+      localStorage.setItem("refreshToken", logInData.refreshToken);
+      localStorage.setItem("uid", logInData.localId);
+      localStorage.setItem("email", userEmail);
+      localStorage.setItem("name", name);
+      dispatch(setLogin({ uid, name, email: userEmail }));
+
+      alert(`Welcome ${name}!`);
       onClose();
     } catch (error: any) {
       alert(error.message);
