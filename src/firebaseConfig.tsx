074d99077636da1b35356ff8ea26c7b2c9dc7ebe
manoj bhatt <enterbhatt123@gmail.com>
2025-09-19 16:33:46 +0530

    feat: resolving error 2

--- src/firebaseConfig.tsx
+++ src/firebaseConfig.tsx
@@ -1,6 +1,6 @@
 
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+// import { getAnalytics } from "firebase/analytics";
 import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
